3933eb8bf4d8d4c157655fc9f2e0e79573e8e218
Perry Xiang <1142320771@qq.com>
2018-03-13 00:00:52 +0800

    react-parcel and remove node_modules

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+import React from 'react';
+import ReactDOM from 'react-dom';
+class HelloMessage extends React.Component{
+    constructor(){
+        super();
+    }
+    render(){
+        return <div>Hello {this.props.name}</div>
+    }
+}
+ReactDOM.render(<HelloMessage name="Perry Xiang"/>,document.getElementById('app'));
